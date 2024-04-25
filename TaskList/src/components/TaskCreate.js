79897dbaab0afbbcbca2fb607ca42273f74a3e18
Can Arda <canarda.dev@gmail.com>
2024-04-25 20:05:46 +0200

    Add files via upload

--- TaskList/src/components/TaskCreate.js
+++ TaskList/src/components/TaskCreate.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
-function TaskCreate({ onCreate }) {
-  const [title, setTitle] = useState("");
-  const [taskDesc, setTaskDesc] = useState("");
+function TaskCreate({ onCreate, task, taskformUpdate, onUpdate }) {
+  const [title, setTitle] = useState(task ? task.title : "");
+  const [taskDesc, setTaskDesc] = useState(task ? task.taskDesc : "");
 
   const handleChange = (event) => {
     setTitle(event.target.value);
@@ -14,28 +14,66 @@ function TaskCreate({ onCreate }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    onCreate(title, taskDesc);
+    if (taskformUpdate) {
+      onUpdate(task.id, title, taskDesc);
+    } else {
+      onCreate(title, taskDesc);
+    }
+
     setTitle("");
     setTaskDesc("");
   };
 
   return (
-    <div className="task-create">
-      <h3>Please add a Text!</h3>
-      <form className="task-form">
-        <label className="task-label">Header</label>
-        <input value={title} onChange={handleChange} className="task-input" />
-        <label className="task-label">Add Text</label>
-        <textarea
-          value={taskDesc}
-          onChange={handleTaskChange}
-          className="task-input"
-          rows={5}
-        />
-        <button className="task-button" onClick={handleSubmit}>
-          Create
-        </button>
-      </form>
+    <div>
+      {taskformUpdate ? (
+        <div className="task-update">
+          <h3>Please Edit The Task</h3>
+          <form className="task-form">
+            <label className="task-label">Please Edit The Header</label>
+            <input
+              value={title}
+              onChange={handleChange}
+              className="task-input"
+            />
+            <label className="task-label">Please Edit The Text</label>
+            <textarea
+              value={taskDesc}
+              onChange={handleTaskChange}
+              className="task-input"
+              rows={5}
+            />
+            <button
+              className="task-button update-button"
+              onClick={handleSubmit}
+            >
+              Edit
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div className="task-create">
+          <h3>Please add a Text!</h3>
+          <form className="task-form">
+            <label className="task-label">Header</label>
+            <input
+              value={title}
+              onChange={handleChange}
+              className="task-input"
+            />
+            <label className="task-label">Add Text</label>
+            <textarea
+              value={taskDesc}
+              onChange={handleTaskChange}
+              className="task-input"
+              rows={5}
+            />
+            <button className="task-button" onClick={handleSubmit}>
+              Create
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
--- TaskList/src/components/TaskList.js
+++ TaskList/src/components/TaskList.js
@@ -1,5 +1,20 @@
-function TaskList() {
-  return <div></div>;
+import TaskShow from "./TaskShow";
+
+function TaskList({ tasks, onDelete, onUpdate }) {
+  return (
+    <div className="task-list">
+      {tasks.map((task, index) => {
+        return (
+          <TaskShow
+            key={index}
+            task={task}
+            onDelete={onDelete}
+            onUpdate={onUpdate}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default TaskList;
